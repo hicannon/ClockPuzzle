1f3cade8e84388bf50a73d57f5f080b3bd716d80
Detian Shi <hi_cannon@yahoo.com>
2014-11-14 15:26:05 -0800

    Win animation

--- ClockPuzzle/clocklibrary/src/main/java/comx/detian/clocklibrary/ClockView.java
+++ ClockPuzzle/clocklibrary/src/main/java/comx/detian/clocklibrary/ClockView.java
@@ -21,16 +21,22 @@ import android.widget.Toast;
 public class ClockView extends View implements Runnable {
     private static final long HANDS_MOVE_TIME = 1000; //millis
     private static final long HANDS_RESET_TIME = 1000;
+    private static final long WINNING_TIME = 2500;
     private static final float NUM_PADDING = 5f;
+
+    public static enum GAME_STATE{
+        HANDS_RESETTING, HANDS_MOVING, WINNING, PLAYING
+    }
+
     private Paint paint;
     private ClockPuzzle data;
+    private GAME_STATE gameState;
 
     private GestureDetectorCompat mDetector;
 
     private PointF[] numCenters = null;
     private float clockRadius;
 
-    private boolean handsReseting = false, handsMoving = false;
     private int oldCWHand = -1;
     private int oldCCWHand = -1;
     private long startMoveTime;
@@ -39,7 +45,8 @@ public class ClockView extends View implements Runnable {
 
     private long gameStartTime;
     private int retries;
-    private int clockSize = 6;
+    private int clockSize = 3;
+    private Vibrator vibrator;
 
     public ClockView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -50,11 +57,18 @@ public class ClockView extends View implements Runnable {
         paint.setColor(Color.WHITE);
         mDetector = new GestureDetectorCompat(context, new InternalGestureListener(this));
 
-        handsMoving = false;
+        gameState = GAME_STATE.PLAYING;
 
         //if (isInEditMode()){
-            setData(new ClockPuzzle(8));
+            setData(new ClockPuzzle(clockSize));
         //}
+
+        try {
+            vibrator = ((Vibrator) getContext().getSystemService(Context.VIBRATOR_SERVICE));
+            vibrator = vibrator.hasVibrator() ? vibrator : null;
+        } catch (Exception e) {
+            vibrator = null;
+        }
 
         this.run();
     }
@@ -77,7 +91,12 @@ public class ClockView extends View implements Runnable {
         retries = 0;
         gameStartTime = System.currentTimeMillis();
         invalidate();
-        ((Vibrator) getContext().getSystemService(Context.VIBRATOR_SERVICE)).vibrate(100);
+        vibrateIfAvailable(200);
+    }
+
+    private void vibrateIfAvailable(int i) {
+        if (vibrator!=null)
+            vibrator.vibrate(i);
     }
 
     private void updateBounds(int cWidth, int cHeight){
@@ -87,11 +106,12 @@ public class ClockView extends View implements Runnable {
 
         separationDegree = (360.0 / data.getSize()) * (Math.PI / 180.0);
         clockRadius = Math.min(cWidth, cHeight)/2-10f;
-        numberRadius = clockRadius / 5.0;
 
         //High school Geometry FTW
         double temp =  Math.sqrt(2 * (clockRadius * clockRadius) - 2 * (clockRadius * clockRadius) * Math.cos(separationDegree))/2.0;
-        numberRadius = (float) Math.min(numberRadius, Math.sqrt(2 * Math.pow(clockRadius - temp, 2) - 2 * Math.pow(clockRadius - temp, 2) * Math.cos(separationDegree)) / 2.0);
+        numberRadius = (float) Math.sqrt(2 * Math.pow(clockRadius - temp, 2) - 2 * Math.pow(clockRadius - temp, 2) * Math.cos(separationDegree)) / 2.0;
+        numberRadius = ((numberRadius < (clockRadius / 5.0)) ? (clockRadius / 5.0) : numberRadius);
+
 
         setTextSizeForCircle(paint, (float) (numberRadius * 2), data.getSize() + "");
 
@@ -164,6 +184,7 @@ public class ClockView extends View implements Runnable {
             return;
         }
 
+        canvas.save(Canvas.MATRIX_SAVE_FLAG); //Saving the canvas and later restoring it so only this image will be rotated.
         canvas.drawColor(Color.BLACK);
 
         float cWidth = canvas.getWidth();
@@ -172,11 +193,42 @@ public class ClockView extends View implements Runnable {
         float centerX = cWidth / 2;
         float centerY = cHeight / 2;
 
+        if (gameState==GAME_STATE.WINNING) {
+            if (System.currentTimeMillis() - startMoveTime > WINNING_TIME) {
+                setData(new ClockPuzzle(clockSize++));
+                gameState = GAME_STATE.PLAYING;
+            }
+
+            long elapsedTime = System.currentTimeMillis() - startMoveTime;
+            float percentage = 1 - (elapsedTime / (float) WINNING_TIME);
+
+            float maxRadius = (float) (clockRadius - 2 * numberRadius - 3 * NUM_PADDING);
+
+            if (percentage < 0.5) {
+                for (float i = 5; i < maxRadius; i += 5) {
+                    paint.setColor(Color.rgb((int) (50 + (i / maxRadius * 200 * (1 - percentage))), 0, (int) (50 + (i / maxRadius * 200 * percentage))));
+                    canvas.drawCircle(centerX, centerY, (i * percentage), paint);
+                }
+            }else{
+                percentage =  1- ((percentage-0.5f) / 0.5f);
+                for (float i = maxRadius; i >0; i -= 5) {
+                    paint.setColor(Color.rgb((int) (50 + (i / maxRadius * 200 * (1 - percentage))), 0, (int) (50 + (i / maxRadius * 200 * percentage))));
+                    canvas.drawCircle(centerX, centerY, (i * percentage), paint);
+                }
+            }
+
+            canvas.rotate(3 * 360 * percentage, canvas.getWidth()/2,canvas.getHeight()/2);
+            //canvas.drawBitmap(yourBitmap, left, top, null);
+            //canvas.restore();
+
+        }else{
+            paint.setColor(Color.BLUE);
+            canvas.drawCircle(centerX, centerY, (float) (clockRadius - 2 * numberRadius - 2 * NUM_PADDING), paint);
+        }
+
         paint.setColor(Color.RED);
         canvas.drawCircle(centerX, centerY, clockRadius, paint);
 
-        paint.setColor(Color.BLUE);
-        canvas.drawCircle(centerX, centerY, (float) (clockRadius - 2 * numberRadius - 3 * NUM_PADDING), paint);
         /*long currentTimeMillis = System.currentTimeMillis();
         double degree = ((currentTimeMillis % 60000) / 60000.0) * 2 * Math.PI - (0.25 * Math.PI);
 
@@ -197,25 +249,28 @@ public class ClockView extends View implements Runnable {
 
             canvas.drawText(data.get(i)+"", numCenterX - (paint.measureText(data.get(i)+"")/2f), numCenterY - ((paint.ascent()+paint.descent())/2f), paint);
 
-            if (handsMoving || handsReseting){
+            if (gameState==GAME_STATE.HANDS_MOVING || gameState==GAME_STATE.HANDS_RESETTING){
                 long elapsedTime = System.currentTimeMillis() - startMoveTime;
-                double percentage = elapsedTime / (double) (handsMoving ? HANDS_MOVE_TIME : HANDS_RESET_TIME);
+                double percentage = elapsedTime / (double) ((gameState==GAME_STATE.HANDS_MOVING) ? HANDS_MOVE_TIME : HANDS_RESET_TIME);
 
                 if (percentage >= 1){
-                    if (handsReseting){
-                        handsReseting = false;
-                        handsMoving = true;
+                    if (gameState==GAME_STATE.HANDS_RESETTING){
+                        gameState = GAME_STATE.HANDS_MOVING;
                         oldCCWHand = data.getCurrentIndex();
                         oldCWHand = data.getCurrentIndex();
                         percentage = 0;
                         startMoveTime = System.currentTimeMillis();
-                    }else if (handsMoving)
-                        handsMoving = false;
+                    }else if (gameState==GAME_STATE.HANDS_MOVING)
+                        gameState = GAME_STATE.PLAYING;
                 }
 
                 paint.setColor(Color.RED);
-                double degreeDistanceCW = percentage * ((handsReseting ? data.getCurrentIndex() : data.getHandCW()) - oldCWHand) * separationDegree;
-                double degreeDistanceCCW = percentage * ((handsReseting ? data.getCurrentIndex() : data.getHandCCW()) - oldCCWHand) * separationDegree;
+
+                double targetCW = ((gameState==GAME_STATE.HANDS_RESETTING) ? data.getCurrentIndex() : data.getHandCW());
+                double targetCCW = ((gameState==GAME_STATE.HANDS_RESETTING) ? data.getCurrentIndex() : data.getHandCCW());
+
+                double degreeDistanceCW = percentage * (targetCW - oldCWHand) * separationDegree;
+                double degreeDistanceCCW = percentage * (targetCCW - oldCCWHand) * separationDegree;
 
                 canvas.drawLine(centerX, centerY, (float) (centerX + Math.cos(separationDegree * oldCCWHand + degreeDistanceCCW) * (clockRadius - numberRadius)), (float) (centerY + Math.sin(degreeDistanceCCW + separationDegree * oldCCWHand) * (clockRadius - numberRadius)), paint);
                 canvas.drawLine(centerX, centerY, (float) (centerX + Math.cos(separationDegree * oldCWHand + degreeDistanceCW) * (clockRadius - numberRadius)), (float) (centerY + Math.sin(degreeDistanceCW + separationDegree * oldCWHand) * (clockRadius - numberRadius)), paint);
@@ -227,6 +282,7 @@ public class ClockView extends View implements Runnable {
             }
         }
 
+        canvas.restore();
     }
 
     @Override
@@ -239,7 +295,7 @@ public class ClockView extends View implements Runnable {
     @Override
     public void run() {
         postInvalidate();
-        if (handsMoving || handsReseting)
+        if (gameState!=GAME_STATE.PLAYING)
             postDelayed(this,33); //around 30fps
     }
 
@@ -264,8 +320,9 @@ public class ClockView extends View implements Runnable {
         public void onLongPress(MotionEvent e){
             Toast.makeText(getContext(), "Retries: "+ (++retries), Toast.LENGTH_SHORT).show();
             data.clearMarks();
+            oldCWHand = -1;
             invalidate();
-            ((Vibrator) cv.getContext().getSystemService(Context.VIBRATOR_SERVICE)).vibrate(200);
+            vibrateIfAvailable(200);
         }
 
         @Override
@@ -297,28 +354,29 @@ public class ClockView extends View implements Runnable {
             if (cv.data.setMarked(index)) {
                 if (data.hasWon()) {
                     Toast.makeText(getContext(), "Won!!!\nRetries: "+ retries+"\nTime: "+(System.currentTimeMillis()-gameStartTime)/1000.0+ "sec", Toast.LENGTH_SHORT).show();
-                    setData(new ClockPuzzle(clockSize++));
+                    startMoveTime = System.currentTimeMillis();
+                    gameState = GAME_STATE.WINNING;
+                    oldCWHand = -1;
                 }else if (data.hasLost()) {
                     Toast.makeText(getContext(), "Lost :(\nRetries: "+ retries++, Toast.LENGTH_SHORT).show();
                     data.clearMarks();
                     oldCWHand = -1;
-                    ((Vibrator) cv.getContext().getSystemService(Context.VIBRATOR_SERVICE)).vibrate(200);
+                    vibrateIfAvailable(200);
                 }else{
-                    handsReseting = true;
+                    gameState = GAME_STATE.HANDS_RESETTING;
                     startMoveTime = System.currentTimeMillis();
-                }
 
-                if (oldCWHand==-1){
-                    oldCWHand = index;
-                    oldCCWHand = index;
-                }else{
-                    oldCCWHand = tempOldCCWHand;
-                    oldCWHand = tempOldCWHand;
-                }
+                    if (oldCWHand==-1){
+                        oldCWHand = index;
+                        oldCCWHand = index;
+                    }else{
+                        oldCCWHand = tempOldCCWHand;
+                        oldCWHand = tempOldCWHand;
+                    }
 
-                if (oldCWHand==index && oldCCWHand==index){
-                    handsReseting = false;
-                    handsMoving = true;
+                    if (oldCWHand==index && oldCCWHand==index){
+                        gameState = GAME_STATE.HANDS_MOVING;
+                    }
                 }
 
                 cv.post(cv);
